f86509fef82650e83ca1e2de8c34df905a96d780
Your Name <you@example.com>
2020-06-18 14:57:55 +0200

    18-06-20

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,70 +16,51 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // your WordPress source
-        baseUrl: `wordpress.kmfurniture.pl`,
-        protocol: `http`,
+        baseUrl: `bartosz-cpp.pl`,
+        protocol: `https`,
         //restApiRoutePrefix: "wp-json",
         // is it hosted on wordpress.com, or self-hosted?
         hostingWPCOM: false,
         // does your site use the Advanced Custom Fields Plugin?
         useACF: true,
         verboseOutput: true,
-        includedRoutes: [
-          "**/categories",
-          "**/posts",
-          "**/pages",
-          "**/media",
-          "**/tags",
-          "**/taxonomies",
-          "**/users",
-          "**/menus",
-          "**/portfolio",
-          "**/services",
-          "**/qualifications",
-          "**/gallery",
-          "**/logo",
-          "**/location",
-        ],
       },
     },
     {
       resolve: "@pasdo501/gatsby-source-woocommerce",
       options: {
         // Base URL of Wordpress site
-        api: "wordpress.kmfurniture.pl/",
+        api: "bartosz-cpp.pl",
 
         // set to false to not see verbose output during build
         // default: true
         verbose: true,
 
         // true if using https. otherwise false.
-        https: false,
+        https: true,
         api_keys: {
-          consumer_key: "ck_3f2fa9654eed764a19a8e04803d0379c7f737c8c",
-          consumer_secret: "cs_91d64ba457780ee5095127b6e625b20feeb78e7e",
+          consumer_key: "ck_ab97695e74efed04c1c8b721d37f9998739ab886",
+          consumer_secret: "cs_ff712f9666b915ea171e57be99d95d7445e3fe5e",
         },
         // Array of strings with fields you'd like to create nodes for...
-        fields: ["products", "products/categories", "products/attributes"],
+        fields: ["products"],
         // Send the API keys as query string parameters instead of using the authorization header
         // OPTIONAL: defaults to false
-        query_string_auth: false,
+        query_string_auth: true,
         // Version of the woocommerce API to use
         // OPTIONAL: defaults to 'wc/v3'
-        api_version: "wc/v3",
+        //api_version: "wc/v3",
         // OPTIONAL: How many results to retrieve *per request*
         per_page: 100,
         // OPTIONAL: Custom WP REST API url prefix, only needed if not using
         // the default wp-json prefix.
-        wpAPIPrefix: "wp-rest",
+        wpAPIPrefix: "wp-json",
         // OPTIONAL: Support for URLs with ports, e.g. 8080; defaults to no port
-        //port: "8000",
+        port: "8000",
         //
         // OPTIONAL: Encoding; default to 'utf8'
         encoding: "utf8",
         // OPTIONAL: Custom Axios config (see https://github.com/axios/axios) - note that this can override other options.
-        axios_config: {
-          // Axios config options
-        },
       },
     },
   ],
